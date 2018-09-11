d58b46114f1467521b5344425050d3393d8fedbc
Databack <rparragalaviz@gmail.com>
2018-09-11 11:55:32 -0700

    tiposDatos y Factorial .cpp

---
+++ tiposDatos.cpp
@@ -0,0 +1,18 @@
+//tiposDatos.cpp
+
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	   // for loop execution
+   for( int a = 10; a < 20; a = a + 1 ) {
+      cout << "value of a: " << a << endl;
+   }
+ 
+   return 0;
+}
